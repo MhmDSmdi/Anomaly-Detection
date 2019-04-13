aa274ba16b38f247e41f6758dcebf07143fcbe7e
mhmdsmdi <mhmd.samadi76@gmail.com>
2019-04-13 20:13:21 +0430

    Model updated

--- dataset/arrhythmia_dataset.py
+++ dataset/arrhythmia_dataset.py
@@ -9,110 +9,115 @@ from deepwalk import walks as serialized_walks
 from deepwalk.skipgram import Skipgram
 
 
-def adj_matrix_to_list(address, node_numbers, output_name):
-    adj_matrix = np.loadtxt(address, usecols=range(node_numbers))
-    adj_list = []
-    for i in range(node_numbers):
-        if adj_matrix[i, i] == 1:
-            adj_list.append([i])
+class ArrhythmiaDataSet:
+
+    def adj_matrix_to_list(self, address, node_numbers, output_name):
+        adj_matrix = np.loadtxt(address, usecols=range(node_numbers))
+        adj_list = []
+        for i in range(node_numbers):
+            if adj_matrix[i, i] == 1:
+                adj_list.append([i])
+            else:
+                adj_list.append([])
+            for j in range(node_numbers):
+                if adj_matrix[i, j] == 1 and i != j:
+                    adj_list[i].append(j)
+        self.create_adj_list_file(adj_list, output_name)
+        return adj_list
+
+    def create_adj_list_file(self, adj_list, file_name):
+        file = open(file_name, 'w')
+        for i in range(len(adj_list)):
+            for j in range(len(adj_list[i])):
+                file.write(str((adj_list[i])[j]))
+                file.write(" ")
+            file.write("\n")
+        file.close()
+
+    def load_graph(self, input_address, output_name="g1_out.embeddings", number_walks=10, walk_length=40,
+                   max_memory_data_size=1000000000, matfile_variable_name="network", format='adjlist', undirected=True,
+                   representation_size=16, workers=1, window_size=5, vertex_freq_degree=False, seed=0):
+        if format == "adjlist":
+            G = graph.load_adjacencylist(input_address, undirected=undirected)
+        elif format == "edgelist":
+            G = graph.load_edgelist(input_address, undirected=undirected)
+        elif format == "mat":
+            G = graph.load_matfile(input_address, variable_name=matfile_variable_name, undirected=undirected)
         else:
-            adj_list.append([])
-        for j in range(node_numbers):
-            if adj_matrix[i, j] == 1 and i != j:
-                adj_list[i].append(j)
-    create_adj_list_file(adj_list, output_name)
-    return adj_list
+            raise Exception("Unknown file format: '%s'.  Valid formats: 'adjlist', 'edgelist', 'mat'" % format)
 
+        print("Number of nodes: {}".format(len(G.nodes())))
 
-def create_adj_list_file(adj_list, file_name):
-    file = open(file_name, 'w')
-    for i in range(len(adj_list)):
-        for j in range(len(adj_list[i])):
-            file.write(str((adj_list[i])[j]))
-            file.write(" ")
-        file.write("\n")
-    file.close()
+        num_walks = len(G.nodes()) * number_walks
 
+        print("Number of walks: {}".format(num_walks))
 
-def load_graph(input_address, output_name="g1_out.embeddings", number_walks=10, walk_length=40,
-               max_memory_data_size=1000000000, matfile_variable_name="network", format='adjlist', undirected=True,
-               representation_size=16, workers=1, window_size=5, vertex_freq_degree=False, seed=0):
-    if format == "adjlist":
-        G = graph.load_adjacencylist(input_address, undirected=undirected)
-    elif format == "edgelist":
-        G = graph.load_edgelist(input_address, undirected=undirected)
-    elif format == "mat":
-        G = graph.load_matfile(input_address, variable_name=matfile_variable_name, undirected=undirected)
-    else:
-        raise Exception("Unknown file format: '%s'.  Valid formats: 'adjlist', 'edgelist', 'mat'" % format)
+        data_size = num_walks * walk_length
 
-    print("Number of nodes: {}".format(len(G.nodes())))
+        print("Data size (walks*length): {}".format(data_size))
 
-    num_walks = len(G.nodes()) * number_walks
-
-    print("Number of walks: {}".format(num_walks))
-
-    data_size = num_walks * walk_length
-
-    print("Data size (walks*length): {}".format(data_size))
-
-    if data_size < max_memory_data_size:
-        print("Walking...")
-        walks = graph.build_deepwalk_corpus(G, num_paths=number_walks,
-                                            path_length=walk_length, alpha=0, rand=random.Random(seed))
-        print("Training...")
-        model = Word2Vec(walks, size=representation_size, window=window_size, min_count=0, sg=1, hs=1, workers=workers)
-    else:
-        print("Data size {} is larger than limit (max-memory-data-size: {}).  Dumping walks to disk.".format(data_size,
-                                                                                                             max_memory_data_size))
-        print("Walking...")
-
-        walks_filebase = output_name + ".walks"
-        walk_files = serialized_walks.write_walks_to_disk(G, walks_filebase, num_paths=number_walks,
-                                                          path_length=walk_length, alpha=0,
-                                                          rand=random.Random(seed),
-                                                          num_workers=workers)
-
-        print("Counting vertex frequency...")
-        if not vertex_freq_degree:
-            vertex_counts = serialized_walks.count_textfiles(walk_files, workers)
+        if data_size < max_memory_data_size:
+            print("Walking...")
+            walks = graph.build_deepwalk_corpus(G, num_paths=number_walks,
+                                                path_length=walk_length, alpha=0, rand=random.Random(seed))
+            print("Training...")
+            model = Word2Vec(walks, size=representation_size, window=window_size, min_count=0, sg=1, hs=1,
+                             workers=workers)
         else:
-            # use degree distribution for frequency in tree
-            vertex_counts = G.degree(nodes=G.iterkeys())
+            print("Data size {} is larger than limit (max-memory-data-size: {}).  Dumping walks to disk.".format(
+                data_size,
+                max_memory_data_size))
+            print("Walking...")
 
-        print("Training...")
-        walks_corpus = serialized_walks.WalksCorpus(walk_files)
-        model = Skipgram(sentences=walks_corpus, vocabulary_counts=vertex_counts,
-                         size=representation_size,
-                         window=window_size, min_count=0, trim_rule=None, workers=workers)
+            walks_filebase = output_name + ".walks"
+            walk_files = serialized_walks.write_walks_to_disk(G, walks_filebase, num_paths=number_walks,
+                                                              path_length=walk_length, alpha=0,
+                                                              rand=random.Random(seed),
+                                                              num_workers=workers)
 
-    model.wv.save_word2vec_format(output_name)
+            print("Counting vertex frequency...")
+            if not vertex_freq_degree:
+                vertex_counts = serialized_walks.count_textfiles(walk_files, workers)
+            else:
+                # use degree distribution for frequency in tree
+                vertex_counts = G.degree(nodes=G.iterkeys())
 
+            print("Training...")
+            walks_corpus = serialized_walks.WalksCorpus(walk_files)
+            model = Skipgram(sentences=walks_corpus, vocabulary_counts=vertex_counts,
+                             size=representation_size,
+                             window=window_size, min_count=0, trim_rule=None, workers=workers)
 
-def prepare_data_set_matrix(matrix_address, node_numbers, output_name):
-    adj_matrix_to_list(matrix_address, node_numbers, "adj_list_{}.txt".format(output_name))
-    load_graph(input_address="./adj_list_{}.txt".format(output_name), output_name="embedding_{}.txt".format(output_name))
-    output_file = open("./output_{}.txt".format(output_name), 'w')
-    file = open("embedding_{}.txt".format(output_name), 'r')
-    line = file.readline()
-    while line:
+        model.wv.save_word2vec_format(output_name)
+
+    def prepare_data_set_matrix(self, matrix_address, node_numbers, output_name):
+        self.adj_matrix_to_list(matrix_address, node_numbers, "adj_list_{}.txt".format(output_name))
+        self.load_graph(input_address="./adj_list_{}.txt".format(output_name),
+                        output_name="embedding_{}.txt".format(output_name))
+        output_file = open("./output_{}.txt".format(output_name), 'w')
+        file = open("embedding_{}.txt".format(output_name), 'r')
         line = file.readline()
-        line = line.split(" ")
-        line.pop(0)
-        for x in line:
-            output_file.write(" " + x)
-    file.close()
-    output_file.close()
+        while line:
+            line = file.readline()
+            line = line.split(" ")
+            line.pop(0)
+            for x in line:
+                output_file.write(" " + x)
+        file.close()
+        output_file.close()
 
 
-def load_dataSet(embedding_file_address, feature_numbers):
-    data = loadmat("./arrhythmia.mat")
+def load_dataSet(test_size, embedding_file_address="./dataset/output_DataSet.txt", feature_numbers=16):
+    data = loadmat("./dataset/arrhythmia.mat")
     y = data['y']
     X = np.loadtxt(embedding_file_address, usecols=range(feature_numbers))
+    X_train = X[: test_size, :]
+    X_test = X[test_size:, :]
+    y_train = y[: test_size]
+    y_test = y[test_size:]
     print(X.shape, len(y))
-    return X, y
+    return (X_train, X_test), (y_train, y_test)
 
 
 if __name__ == '__main__':
-    # prepare_data_set_matrix("./adj.txt", 452, "DataSet")
-    load_dataSet("./output_DataSet.txt", 16)
+    load_dataSet(120)
--- vae_keras.py
+++ vae_keras.py
@@ -11,6 +11,9 @@ from keras.datasets import mnist
 from keras.layers import Lambda, Input, Dense
 from keras.losses import binary_crossentropy
 from keras.models import Model
+from dataset.arrhythmia_dataset import ArrhythmiaDataSet
+from dataset.arrhythmia_dataset import load_dataSet
+
 
 def sampling(args):
     z_mean, z_log_var = args
@@ -21,74 +24,13 @@ def sampling(args):
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
 
-def plot_results(models,
-                 data,
-                 batch_size=128,
-                 model_name="vae_mnist"):
-
-    encoder, decoder = models
-    x_test, y_test = data
-    os.makedirs(model_name, exist_ok=True)
-
-    filename = os.path.join(model_name, "vae_mean.png")
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(x_test,
-                                   batch_size=batch_size)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
-    plt.colorbar()
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.savefig(filename)
-    plt.show()
-
-    filename = os.path.join(model_name, "digits_over_latent.png")
-    # display a 30x30 2D manifold of digits
-    n = 30
-    digit_size = 28
-    figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = np.linspace(-4, 4, n)
-    grid_y = np.linspace(-4, 4, n)[::-1]
-
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[i * digit_size: (i + 1) * digit_size,
-            j * digit_size: (j + 1) * digit_size] = digit
-
-    plt.figure(figsize=(10, 10))
-    start_range = digit_size // 2
-    end_range = (n - 1) * digit_size + start_range + 1
-    pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range_x = np.round(grid_x, 1)
-    sample_range_y = np.round(grid_y, 1)
-    plt.xticks(pixel_range, sample_range_x)
-    plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.imshow(figure, cmap='Greys_r')
-    plt.savefig(filename)
-    plt.show()
-
-
-# MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-image_size = x_train.shape[1]
-original_dim = image_size * image_size
-x_train = np.reshape(x_train, [-1, original_dim])
-x_test = np.reshape(x_test, [-1, original_dim])
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
-
+# Arrhythmia dataset
+(x_train, x_test), (y_train, y_test) = load_dataSet(test_size=120)
+feature_size = x_train.shape[1]
 # network parameters
-input_shape = (original_dim,)
+input_shape = (feature_size, )
 intermediate_dim = 512
-batch_size = 128
+batch_size = 5
 latent_dim = 2
 epochs = 50
 
@@ -111,7 +53,7 @@ encoder.summary()
 # build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
 x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-outputs = Dense(original_dim, activation='sigmoid')(x)
+outputs = Dense(feature_size, activation='sigmoid')(x)
 
 # instantiate decoder model
 decoder = Model(latent_inputs, outputs, name='decoder')
@@ -122,7 +64,7 @@ vae = Model(inputs, outputs, name='vae_mlp')
 models = (encoder, decoder)
 data = (x_test, y_test)
 reconstruction_loss = binary_crossentropy(inputs, outputs)
-reconstruction_loss *= original_dim
+reconstruction_loss *= feature_size
 kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
 kl_loss = K.sum(kl_loss, axis=-1)
 kl_loss *= -0.5
@@ -135,9 +77,4 @@ vae.fit(x_train,
         epochs=epochs,
         batch_size=batch_size,
         validation_data=(x_test, None))
-vae.save_weights('vae_mlp_mnist.h5')
-
-plot_results(models,
-             data,
-             batch_size=batch_size,
-             model_name="vae_mlp")
+vae.save_weights('vae_mlp_arrhythmia.h5')
